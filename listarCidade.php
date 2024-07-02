88199442e263241d83ab2dbeec8cba725d4338a5
Stephia <stephaniefhatida@hotmail.com>
2024-07-02 11:35:10 -0300

    update triste

---
+++ listarCidade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista</title>
+</head>
+<body>
+    <?php
+        include('includes/conexao.php');
+
+        $sql = "SELECT * FROM cidade";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($result);
+    ?>
+    <h1>Consulta de Cidades</h1>
+    <a href="CadastroCidade.html">Cadastrar Nova Cidade</a>
+    <table align="center" border="1" width="500">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Estado</th>
+        </tr>
+        <?php
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                echo "<td>".$row['id']."</td>";
+                echo "<td>".$row['nome']."</td>";
+                echo "<td>".$row['estado']."</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
